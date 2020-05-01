6517e1e8f5f4b56f41abd3f2896a01023e26f7b7
Seidex <olehchabak0410@gmail.com>
2020-05-01 21:51:40 +0300

    add lab 2.1

---
+++ lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; string res = "boi"; bool ok = true;
+            Console.WriteLine("Write your month");
+            a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1: res = "winter"; break;
+                case 2: res = "winter"; break;
+                case 3: res = "spring"; break;
+                case 4: res = "spring"; break;
+                case 5: res = "spring"; break;
+                case 6: res = "summer"; break;
+                case 7: res = "summer"; break;
+                case 8: res = "summer"; break;
+                case 9: res = "autumn"; break;
+                case 10: res = "autumn"; break;
+                case 11: res = "autumn"; break;
+                case 12: res = "winter"; break;
+                default: ok = false; break;
+            }
+            if (ok) Console.WriteLine("Time of the year: " + res);
+            else Console.WriteLine("Incorrect meaning");
+        }
+    }
+}
